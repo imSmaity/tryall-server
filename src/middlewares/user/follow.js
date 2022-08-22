da3f24cda7987d14b993dacee77b68d0c807f673
Suman <60940736+imSmaity@users.noreply.github.com>
2022-08-23 01:11:15 +0530

    Follow and Unfollow

--- src/middlewares/user/follow.js
+++ src/middlewares/user/follow.js
@@ -36,3 +36,63 @@ module.exports.getFollowers = async (req, res) => {
 		res.status(400).send({ msg: 'Something went wrong!' });
 	}
 };
+module.exports.getUnknownUsers = async (req, res) => {
+	const { id } = req.params;
+	const user = await User.findById(id);
+	try {
+		if (user) {
+			const users = await User.find();
+			const unknownUsers = users.filter((u) => {
+				return user.following.indexOf(u._id) === -1;
+			});
+			res.status(200).send(unknownUsers);
+		} else {
+			throw Error();
+		}
+	} catch {
+		res.status(400).send({ msg: 'Something went wrong!' });
+	}
+};
+
+module.exports.follow = async (req, res) => {
+	const { id, followUserId } = req.body;
+	const user = await User.findById(id);
+	try {
+		user.following.unshift(followUserId);
+		const updatedUser = await User.findByIdAndUpdate(user._id, user, {
+			new: true,
+		});
+		const users = await User.find();
+		const updatedUsers = users.filter((u) => {
+			return updatedUser.following.indexOf(u._id) === -1;
+		});
+
+		res.send({ user: updatedUser, users: updatedUsers });
+	} catch {
+		res.status(400).send({ msg: 'Something went wrong!' });
+	}
+};
+module.exports.unfollow = async (req, res) => {
+	const { id, unfollowUserId } = req.body;
+
+	const user = await User.findById(id);
+	try {
+		const index = user.following.indexOf(unfollowUserId);
+		if (index !== -1) {
+			user.following.splice(unfollowUserId);
+			const updatedUser = await User.findByIdAndUpdate(user._id, user, {
+				new: true,
+			});
+			const users = await User.find();
+			const updatedUsers = users.filter((u) => {
+				return updatedUser.following.indexOf(u._id) === -1;
+			});
+
+			res.send({ user: updatedUser, users: updatedUsers });
+		} else {
+			throw Error();
+		}
+	} catch {
+		res.status(400).send({ msg: 'Something went wrong!' });
+	}
+};
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
-const { getFollowing, getFollowers } = require('../middlewares/user/follow');
+const {
+	getFollowing,
+	getFollowers,
+	getUnknownUsers,
+	follow,
+	unfollow,
+} = require('../middlewares/user/follow');
 
+router.post('/follow', follow);
+router.post('/unfollow', unfollow);
+router.get('/unknown-users/:id', getUnknownUsers);
 router.get('/following/:id', getFollowing);
 router.get('/followers/:id', getFollowers);
 
